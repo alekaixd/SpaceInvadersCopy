e515762587d355efe2709ebfd387a2bc1ce14df3
alekaixd <aleksi.pirttinen@gmail.com>
2023-02-01 13:28:19 +0200

    dooing teh do

---
+++ SpaceInvadersCopy/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [Range(-1, 1)]
+    public float scrollSpeed;
+    private float offset;
+    private Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
